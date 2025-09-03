bbe484b371d1ca49393733b272260da69322b666
ag3ntk1wi <jmalate019@gmail.com>
2025-09-03 23:06:33 +0800

    Initial HomePage Setup

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weborg.views import HomePageView
+from weborg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
--- projectsite/weborg/views.py
+++ projectsite/weborg/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from weborg.models import Priority
 
 # Create your views here.
+class HomePageView(ListView):
+    model = Priority
+    context_object_name = 'home'
+    template_name = "home.html"
