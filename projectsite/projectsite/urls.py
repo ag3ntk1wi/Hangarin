ce95d8ce3dbdf575637cd676ac696c1e82f7eccc
ag3ntk1wi <jmalate019@gmail.com>
2025-10-15 08:16:58 +0800

    added PWA

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -25,6 +25,7 @@ from weborg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pwa.urls')),
     path("accounts/", include("allauth.urls")), # allauth routes
     path('', views.HomePageView.as_view(), name='home'),
     # Priority Paths
